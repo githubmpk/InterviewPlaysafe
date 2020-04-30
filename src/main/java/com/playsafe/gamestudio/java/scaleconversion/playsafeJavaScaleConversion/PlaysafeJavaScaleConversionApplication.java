e5db9e9316899381745a838ebac894af72fc3f02
Kirthan <kirthan.puttegowda@pathcare.org>
2020-04-30 11:17:29 +0200

    restcontroller added due to 404 error

---
+++ src/main/java/com/playsafe/gamestudio/java/scaleconversion/playsafeJavaScaleConversion/PlaysafeJavaScaleConversionApplication.java
@@ -0,0 +1,13 @@
+package com.playsafe.gamestudio.java.scaleconversion.playsafeJavaScaleConversion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaysafeJavaScaleConversionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlaysafeJavaScaleConversionApplication.class, args);
+    }
+
+}
--- src/main/java/com/playsafe/gamestudio/java/scaleconversion/playsafeJavaScaleConversion/controller/ScaleConversionController.java
+++ src/main/java/com/playsafe/gamestudio/java/scaleconversion/playsafeJavaScaleConversion/controller/ScaleConversionController.java
@@ -3,7 +3,9 @@ package com.playsafe.gamestudio.java.scaleconversion.playsafeJavaScaleConversion
 import com.playsafe.gamestudio.java.scaleconversion.playsafeJavaScaleConversion.Bean.KelvinToCelciusBean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class ScaleConversionController {
     @GetMapping(path ="/conversions/ktoc")
     public KelvinToCelciusBean kelvinToCelciusBean(@RequestParam(value = "temperatureKelvin") float temperatureKelvin){
