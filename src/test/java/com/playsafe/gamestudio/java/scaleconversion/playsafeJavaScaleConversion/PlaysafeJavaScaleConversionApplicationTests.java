74b95b55d4ca80d16ae8d4655dc56ed77698b27a
Kirthan <kirthan.puttegowda@pathcare.org>
2020-04-30 10:58:43 +0200

    initial commit

---
+++ src/test/java/com/playsafe/gamestudio/java/scaleconversion/playsafeJavaScaleConversion/PlaysafeJavaScaleConversionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playsafe.gamestudio.java.scaleconversion.playsafeJavaScaleConversion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaysafeJavaScaleConversionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
